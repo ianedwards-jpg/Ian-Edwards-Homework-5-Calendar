475dde53f10954986e4ce39ef6ebdf19b150e09e
Ian Edwards <ian.edward@uconn.edu>
2020-03-12 18:57:15 -0400

    update

--- New folder/Resources/appendLists/script.js
+++ New folder/Resources/appendLists/script.js
@@ -3,7 +3,7 @@ var todoForm = document.querySelector("#todo-form");
 var hoursList = document.querySelector("#hourslist");
 var todoCountSpan = document.querySelector("#todo-count");
 
-var businessHours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+var businessHours = ["9AM:", "10AM:", "11AM:", "12PM:", "1PM:", "2PM:", "3PM:", "4PM:", "5PM:"];
 
 displayHours();
 
@@ -14,10 +14,19 @@ function displayHours() {
 
   // Render a new li for each todo
   for (var i = 0; i < businessHours.length; i++) {
+
+    var row = document.createElement("row")
+    hoursList.appendChild(row);
    
     var li = document.createElement("li");
-    li.setAttribute("type", "text");
+    //li.setAttribute("type", "text");
+    li.className += "col-2"
     li.textContent = businessHours[i];
-    hoursList.appendChild(li);
+    row.appendChild(li);
+
+    var inp = document.createElement("input")
+    inp.setAttribute("type", "text");
+    inp.className += "col-10"
+    row.appendChild(inp)
   }
 }
